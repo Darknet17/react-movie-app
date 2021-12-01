a4b3a380bfa2af60abe07aae190a5259236554a8
Luis Josue Santana <josuesantana17@gmail.com>
2021-12-01 10:46:05 -0400

    ApiServices unit test

---
+++ src/tests/src/core/services/api.services.test.ts
@@ -0,0 +1,36 @@
+import axios from 'axios';
+import ApiService from '../../../../core/services/api.service';
+
+describe('test in ApiServices', ()=> {
+    // starting the axios url base
+    ApiService.init();
+
+    test('checking for a configured url base', ()=> {
+        const url:any = axios.defaults.baseURL;
+        expect(url.split(":")[0]).toBe('https');
+    });
+
+    test('Testing ApiService.query()', async ()=>{
+        const payload: Object = {
+            api_key: process.env.REACT_APP_API_KEY,
+            language: 'en-US',
+            query: 'Capitan America',
+            page: '',
+            include_adult: true,
+            region: 'en-Us',
+            year:'',
+            primary_release_year: ''
+        }
+        const { data } = await ApiService.query({ resource: '/search/movie', params: { params: payload } });
+        const { results } = await data;
+        expect(results?.length).toBe(8);
+    });
+
+    test('Testing ApiService.get()', async ()=>{
+        const { data } = await ApiService.get(
+            { resource: '/movie', slug: `1885/reviews?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1`}
+        );
+        const { results } = await data;
+        expect(results.length).toBe(results.length);
+    });
+});
